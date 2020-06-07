19806721965711b48179955822b4ec8c937dbbe7
James Lovejoy <jameslovejoy1@gmail.com>
2020-06-07 18:02:02 -0400

    Make fnv1a static inline so that Python 2 bindings work

--- h2.c
+++ h2.c
@@ -18,7 +18,7 @@
 const char* dat_file_name = "verthash.dat";
 const char* input_header_hex = "000000203a297b4b7685170d7644b43e5a6056234cc2414edde454a87580e1967d14c1078c13ea916117b0608732f3f65c2e03b81322efc0a62bcee77d8a9371261970a58a5a715da80e031b02560ad8";
 
-inline uint32_t fnv1a(const uint32_t a, const uint32_t b) {
+static inline uint32_t fnv1a(const uint32_t a, const uint32_t b) {
     return (a ^ b) * 0x1000193;
 }
 
